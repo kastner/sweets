91f5e714362f94b429b6a608fb7481b38593da6e
Erik Kastner <kastner@gmail.com>
2009-03-20 11:34:40 -0400

    logout link

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ ActionController::Routing::Routes.draw do |map|
   map.resource :project, :has_many => :filters
   map.resources :sessions, :only => [:create, :destroy]
   map.resources :output, :only => [:show]
+  map.logout '/logout', :controller => "sessions", :action => "destroy"
 end
